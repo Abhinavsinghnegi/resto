e55a5c221f63e0c807fc1096ac4445b271837a70
AbhinavTricon <abhinav.negi@triconinfotech.com>
2022-12-21 11:40:10 +0530

    feat:fetch data from API and set navbar

--- src/app/list-resto/list-resto.component.ts
+++ src/app/list-resto/list-resto.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RestoService } from '../services/resto.service';
 
 @Component({
   selector: 'app-list-resto',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListRestoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private resto : RestoService) { }
+
+  collection:any={};
 
   ngOnInit(): void {
+    this.resto.getList().subscribe((result) => {
+      this.collection = result;
+    });
   }
 
 }
